414e6c47930383244339aa130279a5aa44e38887
LKD70 <lkdornan70@gmail.com>
2017-07-06 09:11:59 +0100

    Updated for FTL
    Removed background image and .js changes

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ while true; do
     read -p "Create a backup of the current files? (Y/n): " yn
     case $yn in
         [Yy]* ) 
-	    cp -f admin/scripts/pi-hole/js/index.js admin/scripts/pi-hole/js/index.bkp.js  # Backup JavaScript...
             cp -f admin/style/vendor/AdminLTE.min.css admin/style/vendor/AdminLTE.min.bkp.css  # Backup StyleSheet...
             echo "Backup completed!"
 			break ;;
@@ -15,13 +14,10 @@ while true; do
     read -p "Install LKD70's Dark Theme now? (Y/n): " yn
     case $yn in
         [Yy]* ) 
-			git clone https://github.com/lkd70/PiHole-Dark.git temp
-            rm -f admin/scripts/pi-hole/js/index.js
-            cp temp/admin/scripts/pi-hole/js/index.js admin/scripts/pi-hole/js/index.js
+	    git clone https://github.com/lkd70/PiHole-Dark.git temp
             rm -f admin/style/vendor/AdminLTE.min.css
             cp temp/admin/style/vendor/AdminLTE.min.css admin/style/vendor/AdminLTE.min.css
             cp temp/admin/style/vendor/AdminLTE.css admin/style/vendor/AdminLTE.css
-            cp temp/admin/img/back.png admin/img/back.png
 	    rm -rf temp/
 			break;;
         [Nn]* ) break;;
